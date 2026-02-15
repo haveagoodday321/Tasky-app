9a2672c03f3c65bce8a7001091a4c68c1e8ba982
haveagoodday321 <sitholesibusiso4132@gmail.com>
2026-02-15 10:56:49 +0200

    Create sw.js

---
+++ docs/sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "tasky-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+// Install service worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch from cache if offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
