a4ed889596c6995bd08c26845c2434a9d73e3d0a
haveagoodday321 <sitholesibusiso4132@gmail.com>
2026-07-11 23:06:53 +0200

    Update premium.js

--- docs/js/premium.js
+++ docs/js/premium.js
@@ -1,1 +1,28 @@
+function unlockPremium(){
+  let trialUsed =
+localStorage.getItem("trialUsed")==="true";
+function unlockPremium(){
 
+if(trialUsed){
+alert("Free trial already used");
+return;
+}
+
+const trialEnd =
+Date.now() + (30*24*60*60*1000);
+
+localStorage.setItem(
+"premiumUntil",
+trialEnd
+);
+
+localStorage.setItem(
+"trialUsed",
+true
+);
+
+alert(
+"🎉 Premium unlocked for 30 days!"
+);
+
+}
