8fe022479e305661fe6c90e6c46f3e21d296c031
haveagoodday321 <sitholesibusiso4132@gmail.com>
2026-07-11 22:42:55 +0200

    Update achievements.js

--- docs/js/achievements.js
+++ docs/js/achievements.js
@@ -1,1 +1,23 @@
+function unlockAchievement(name){
 
+if(
+achievements.includes(name)
+) return;
+
+achievements.push(name);
+
+localStorage.setItem(
+"achievements",
+JSON.stringify(achievements)
+);
+
+renderAchievements();
+
+confetti();
+
+alert(
+"🏆 Achievement Unlocked: "
++ name
+);
+
+}
