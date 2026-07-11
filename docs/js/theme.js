603782e40536042c7816648ae81153cdde9592a9
haveagoodday321 <sitholesibusiso4132@gmail.com>
2026-07-11 22:50:51 +0200

    Update theme.js

--- docs/js/theme.js
+++ docs/js/theme.js
@@ -1,1 +1,21 @@
+function toggleTheme(){
 
+document.body.classList.toggle("light");
+
+if(document.body.classList.contains("light")){
+
+localStorage.setItem(
+"theme",
+"light"
+);
+
+}else{
+
+localStorage.setItem(
+"theme",
+"dark"
+);
+
+}
+
+}
