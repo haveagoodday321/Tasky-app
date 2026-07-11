8f0923859edee4e965250d768108bd260e5b9a6f
haveagoodday321 <sitholesibusiso4132@gmail.com>
2026-07-11 22:37:00 +0200

    Update notifications.js

--- docs/js/notifications.js
+++ docs/js/notifications.js
@@ -1,1 +1,60 @@
+function checkReminders(){
+
+tasks.forEach(task=>{
+
+if(task.deadline){
+
+const due =
+new Date(task.deadline);
+
+if(
+due < new Date()
+&& !task.done
+){
+
+new Notification(
+"🚨 Overdue Task",
+{
+body: task.text
+}
+);
+
+}
+
+}
+
+if(
+task.time &&
+!task.done
+){
+
+const now =
+new Date();
+
+const current =
+now.getHours()
+.toString()
+.padStart(2,"0")
++
+":" +
+now.getMinutes()
+.toString()
+.padStart(2,"0");
+
+if(current===task.time){
+
+new Notification(
+"⏰ Task Reminder",
+{
+body: task.text
+}
+);
+
+}
+
+}
+
+});
+
+}
 
