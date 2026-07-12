4bcf85eeaac6d802fbe1b3c2a02d1440976b62c6
haveagoodday321 <sitholesibusiso4132@gmail.com>
2026-07-12 06:20:44 +0200

    Update exports.js

--- docs/js/exports.js
+++ docs/js/exports.js
@@ -1,1 +1,22 @@
+function exportTasks(){
+  
+const data =
+JSON.stringify(tasks);
 
+const blob =
+new Blob([data],{
+type:"application/json"
+});
+
+const a =
+document.createElement("a");
+
+a.href =
+URL.createObjectURL(blob);
+
+a.download =
+"tasky-backup.json";
+
+a.click();
+
+}
